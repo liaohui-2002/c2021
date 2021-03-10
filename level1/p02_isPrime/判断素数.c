1a16f5e1de8f07bc61628a849475a3e4b234a279
liaohui <lh>
2021-03-10 21:35:21 +0800

    完成 isPrime

---
+++ level1/p02_isPrime/判断素数.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+//int main01()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int flag = 0;
+//	scanf_s("%d", &n);
+//
+//	if (n > 3)
+//	{
+//		for (i = 2; i < sqrt((double)n); i++)
+//		{
+//			if (n % i == 0) {
+//				flag = 1;
+//				break;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			printf("不是素数");
+//		}
+//		else
+//		{
+//			printf("是素数");
+//		}
+//	}
+//	if (n <= 3)
+//		printf("error");
+//
+//	return 0;
+//}
