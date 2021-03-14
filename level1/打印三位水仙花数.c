e0c181f429eb89face2e0a081f66b75ac4c7c5ca
liaohui <lh>
2021-03-14 10:47:32 +0800

    完成narcissus

---
+++ level1/打印三位水仙花数.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+//#include<math.h>
+//
+//void main()
+//{
+//	int a, b, c;
+//	int x = 0;
+//	for (int i = 100; i < 1000; i++)
+//	{
+//		a = i % 10;
+//		b = i % 100 / 10;
+//		c = i % 1000 / 100;
+//		x = pow(a, 3) + pow(b, 3) + pow(c, 3);
+//		if (i == x)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//}
