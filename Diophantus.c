3b55745a2312957caa6cdefaa2f1852c12ab80b3
liaohui <lh>
2021-03-14 11:24:09 +0800

    完成 Diophantus

---
+++ Diophantus.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x = 0; 
+	int ret = 0;//设丢图翻的年龄为x
+	//x = x / 6 + x / 12 + x / 7 + 5 + x / 2 + 4;
+	//由方程
+	x = 756 / 9;
+	ret = x - 4;
+	printf("儿子死时他的年龄是：%d", ret);
+	return 0;
+}
