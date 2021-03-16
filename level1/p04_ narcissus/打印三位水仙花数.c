3b078ae07576312a84215f978e4a792066fb70d0
liaohui <lh>
2021-03-16 15:01:26 +0800

    完成 narcissus

--- level1/p04_ narcissus/打印三位水仙花数.c
+++ level1/p04_ narcissus/打印三位水仙花数.c
@@ -1,19 +1,19 @@
-//#include<stdio.h>
-//#include<math.h>
-//
-//void main()
-//{
-//	int a, b, c;
-//	int x = 0;
-//	for (int i = 100; i < 1000; i++)
-//	{
-//		a = i % 10;
-//		b = i % 100 / 10;
-//		c = i % 1000 / 100;
-//		x = pow(a, 3) + pow(b, 3) + pow(c, 3);
-//		if (i == x)
-//		{
-//			printf("%d ", i);
-//		}
-//	}
-//}
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+	int a, b, c;
+	int x = 0;
+	for (int i = 100; i < 1000; i++)
+	{
+		a = i % 10;//个位
+		b = i % 100 / 10;//十位
+		c = i % 1000 / 100;//百位
+		x = pow(a, 3) + pow(b, 3) + pow(c, 3);
+		if (i == x)
+		{
+			printf("%d ", i);
+		}
+	}
+}
